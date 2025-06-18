de7364e6e60f1f4a43136d86502e816d5b3caa19
sajid khan <sajidmansuri9630@gmail.com>
2025-06-18 08:11:57 +0530

    chanches for responsive

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -5,10 +5,16 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-success">
       <div className="container-fluid">
-        <Link className="navbar-brand fw-bold" to="/">
-          🌾 Gour Krashi Seva Kendra
+        {/* Logo / Brand Name - Responsive Text */}
+        <Link
+          className="navbar-brand fw-bold text-wrap"
+          style={{ whiteSpace: "normal", maxWidth: "200px" }}
+          to="/"
+        >
+          🌾 Gour krashi seva kendra
         </Link>
 
+        {/* Hamburger Button for Mobile View */}
         <button
           className="navbar-toggler"
           type="button"
@@ -21,34 +27,25 @@ function Navbar() {
           <span className="navbar-toggler-icon"></span>
         </button>
 
+        {/* Nav Links */}
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link" to="/">
-                Home
-              </Link>
+              <Link className="nav-link" to="/">Home</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/products">
-                Products
-              </Link>
+              <Link className="nav-link" to="/products">Products</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/about">
-                About
-              </Link>
+              <Link className="nav-link" to="/about">About</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/contact">
-                Contact
-              </Link>
+              <Link className="nav-link" to="/contact">Contact</Link>
             </li>
           </ul>
 
-        
-          <div className="d-flex ms-auto">
-            <DarkMode />
-          </div>
+          {/* Dark Mode Toggle */}
+          <DarkMode />
         </div>
       </div>
     </nav>
